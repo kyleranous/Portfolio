91ad43160dbcb6bd10f454c7f6efd4879548b055
Kyle Ranous <kranous@SM.local>
2021-02-24 07:35:13 -0700

    Added Templates Directory, Base HTML Template, and index.html to project

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': "Kyle" }
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Testy'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Testy McTesterson'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
